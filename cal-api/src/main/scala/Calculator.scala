3f9d8bb650ade2ab8ace75cc817373c43917b823
nikhilkadam46 <nikhil.kadam@knoldus.com>
2021-05-11 15:31:08 +0530

    cal-api commit

---
+++ cal-api/src/main/scala/Calculator.scala
@@ -0,0 +1,10 @@
+class Calculator {
+  def +(a: Int, b: Int): Int = a + b
+
+  def -(a: Int, b: Int): Int = a - b
+
+  def *(a: Int, b: Int): Long = a * b
+
+  def /(a: Int, b: Int): Int = a / b
+
+}
